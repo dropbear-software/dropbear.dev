4e084323115b6f8ea0d2350a636df698b7823e6c
mark-dropbear <76698672+mark-dropbear@users.noreply.github.com>
2023-07-26 21:51:00 +0000

    Fix Typescript type issue

--- src/components/main-navigation/main-navigation.ts
+++ src/components/main-navigation/main-navigation.ts
@@ -37,7 +37,7 @@ export class MainNavigation extends BaseElement {
 
   #renderButton(): TemplateResult {
     return html`
-      <button type="button" aria-expanded="${this.isOpen.toString()}" aria-label="Menu" aria-controls="mainnav" @click="${this.#handleButtonClick}">
+      <button type="button" aria-expanded="${this.isOpen}" aria-label="Menu" aria-controls="mainnav" @click="${this.#handleButtonClick}">
         <svg width="24" height="24" aria-hidden="true">
           <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z">
         </svg>
