8636201f0f44d5585d0d87434b4f9a9cf1f484cf
mark-dropbear <76698672+mark-dropbear@users.noreply.github.com>
2023-08-16 15:36:36 +0000

    Serve HTML

---
+++ functions/homepage.js
@@ -0,0 +1,12 @@
+export const homepageContent = `
+<html>
+  <head>
+    <title>Dropbear.dev</title>
+    <meta name="robots" content="noindex">
+  </head>
+  <body>
+    <h1>Dropbear: Modern Web Consultancy</h1>
+    <p>Coming Soon!</p>
+  </body>
+</html>
+`;
--- functions/index.js
+++ functions/index.js
@@ -9,8 +9,9 @@
 
 import logger from 'firebase-functions/logger';
 import { onRequest } from 'firebase-functions/v2/https';
+import { homepageContent } from './homepage.js';
 
 export const helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+  logger.info("Serving Home Page", {structuredData: true});
+  response.send(homepageContent);
 });
