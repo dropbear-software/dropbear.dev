f26b7d977549beaec0c4ea313e0f199fafbc55be
mark-dropbear <76698672+mark-dropbear@users.noreply.github.com>
2023-08-17 22:26:18 +0000

    Use express app for backend

--- packages/website-server/src/index.ts
+++ packages/website-server/src/index.ts
@@ -11,11 +11,18 @@ import {onRequest} from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
 import {homepageContent} from "./homepage.js";
 import {generateNonce} from "./csp.js";
+import "@google-cloud/functions-framework";
+import express from "express";
 
-export const helloWorld = onRequest((request, response) => {
+const app = express();
+
+app.get('/', (request, response) => {
   const nonceValue = generateNonce();
   logger.info("Serving Home Page", {structuredData: true});
   response.set("Content-Security-Policy", `object-src 'none'; script-src 'nonce-${nonceValue}' 'unsafe-inline' 'strict-dynamic' https: http:; base-uri 'none'; font-src fonts.gstatic.com; style-src 'nonce-${nonceValue}' 'self' fonts.googleapis.com; report-uri https://www.dropbear.dev/services/report-collector/`);
 
   response.send(homepageContent(nonceValue));
 });
+ 
+
+export const helloWorld = onRequest(app);
