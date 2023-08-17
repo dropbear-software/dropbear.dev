452c4d3f08a745f94392c7db5d9371a8db9a3289
mark-dropbear <76698672+mark-dropbear@users.noreply.github.com>
2023-08-17 19:13:23 +0000

    Integrate firebase

---
+++ packages/web-frontend/src/firebase-config.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD3dTU4IFCav2DHb8kWXMqo82uynNNJpOg",
+  authDomain: "dropbear-website-prod.firebaseapp.com",
+  projectId: "dropbear-website-prod",
+  storageBucket: "dropbear-website-prod.appspot.com",
+  messagingSenderId: "162976027192",
+  appId: "1:162976027192:web:0cf5cd1eb837ddb35c0ccd",
+  measurementId: "G-ZREYJ37R8R"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
--- packages/website-server/src/homepage.ts
+++ packages/website-server/src/homepage.ts
@@ -8,6 +8,8 @@ export const homepageContent = (nonce: string) => {
     <title>Dropbear.dev</title>
     <link rel="stylesheet" href="/packages/design-system/css/lib/material-theme/index.css" nonce="${nonce}">
     <meta name="robots" content="noindex">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script nonce="${nonce}">window.dataLayer = [];</script>
   </head>
   <body class="surface on-surface-text">
     <home-page></home-page>
@@ -19,6 +21,8 @@ export const homepageContent = (nonce: string) => {
         "@material/web/button/filled-button.js": "https://ga.jspm.io/npm:@material/web@1.0.0-pre.15/button/filled-button.js",
         "@material/web/button/outlined-button.js": "https://ga.jspm.io/npm:@material/web@1.0.0-pre.15/button/outlined-button.js",
         "@webcomponents/template-shadowroot/template-shadowroot.js": "https://ga.jspm.io/npm:@webcomponents/template-shadowroot@0.2.1/template-shadowroot.js",
+        "firebase/app": "https://ga.jspm.io/npm:firebase@10.1.0/app/dist/esm/index.esm.js",
+        "firebase/analytics": "https://ga.jspm.io/npm:firebase@10.1.0/analytics/dist/esm/index.esm.js",
         "lit": "https://ga.jspm.io/npm:lit@2.7.6/index.js",
         "lit/decorators.js": "https://ga.jspm.io/npm:lit@2.7.6/decorators.js",
         "lit/directives/class-map.js": "https://ga.jspm.io/npm:lit@2.7.6/directives/class-map.js",
@@ -28,8 +32,15 @@ export const homepageContent = (nonce: string) => {
       },
       "scopes": {
         "https://ga.jspm.io/": {
+          "@firebase/analytics": "https://ga.jspm.io/npm:@firebase/analytics@0.10.0/dist/esm/index.esm2017.js",
+          "@firebase/app": "https://ga.jspm.io/npm:@firebase/app@0.9.15/dist/esm/index.esm2017.js",
+          "@firebase/component": "https://ga.jspm.io/npm:@firebase/component@0.6.4/dist/esm/index.esm2017.js",
+          "@firebase/installations": "https://ga.jspm.io/npm:@firebase/installations@0.6.4/dist/esm/index.esm2017.js",
+          "@firebase/logger": "https://ga.jspm.io/npm:@firebase/logger@0.4.0/dist/esm/index.esm2017.js",
+          "@firebase/util": "https://ga.jspm.io/npm:@firebase/util@1.9.3/dist/index.esm2017.js",
           "@lit/reactive-element": "https://ga.jspm.io/npm:@lit/reactive-element@1.6.3/development/reactive-element.js",
           "@lit/reactive-element/decorators/": "https://ga.jspm.io/npm:@lit/reactive-element@1.6.3/development/decorators/",
+          "idb": "https://ga.jspm.io/npm:idb@7.1.1/build/index.js",
           "lit-element/lit-element.js": "https://ga.jspm.io/npm:lit-element@3.3.3/development/lit-element.js",
           "lit-html": "https://ga.jspm.io/npm:lit-html@2.8.0/development/lit-html.js",
           "lit-html/": "https://ga.jspm.io/npm:lit-html@2.8.0/development/"
@@ -44,6 +55,7 @@ export const homepageContent = (nonce: string) => {
     </script>
     <script async src="https://ga.jspm.io/npm:es-module-shims@1.8.0/dist/es-module-shims.js" crossorigin="anonymous" nonce="${nonce}"></script>
     <script type="module" src="/packages/web-frontend/lib/pages/home/homepage.js" nonce="${nonce}"></script>
+    <script type="module" src="/packages/web-frontend/lib/firebase-config.js" nonce="${nonce}"></script>
   </body>
 </html>
 `;
