bc09ff62d05625e6413b80822a6685ae3cd67ec8
mark-dropbear <76698672+mark-dropbear@users.noreply.github.com>
2023-08-17 13:13:48 +0000

    Hook up wire it. Closes #7

--- packages/website-server/.eslintrc.cjs
+++ packages/website-server/.eslintrc.cjs
@@ -19,6 +19,7 @@ module.exports = {
   },
   ignorePatterns: [
     "/lib/**/*", // Ignore built files.
+    "/types/**/*", // Ignore type files.
   ],
   plugins: [
     "@typescript-eslint",
--- packages/website-server/src/homepage.ts
+++ packages/website-server/src/homepage.ts
@@ -1,12 +1,13 @@
 export const homepageContent = `
 <html>
   <head>
+    <meta charset="UTF-8">
     <title>Dropbear.dev</title>
     <meta name="robots" content="noindex">
   </head>
   <body>
     <h1>Dropbear: Modern Web Consultancy</h1>
-    <p>Coming Soon!!!</p>
+    <p>Coming Soon!</p>
   </body>
 </html>
 `;
