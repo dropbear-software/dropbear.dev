e1638690dea1b17086f0476bc9820c8aa18fb990
mark <mark@dropbear.dev>
2024-08-05 14:03:38 +0000

    Fresh slate

---
+++ .dart_tool/build/entrypoint/build.dart
@@ -0,0 +1,52 @@
+// ignore_for_file: directives_ordering
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:build_runner_core/build_runner_core.dart' as _i1;
+import 'package:source_gen/builder.dart' as _i2;
+import 'package:functions_framework_builder/builder.dart' as _i3;
+import 'package:build_resolvers/builder.dart' as _i4;
+import 'dart:isolate' as _i5;
+import 'package:build_runner/build_runner.dart' as _i6;
+import 'dart:io' as _i7;
+
+final _builders = <_i1.BuilderApplication>[
+  _i1.apply(
+    r'source_gen:combining_builder',
+    [_i2.combiningBuilder],
+    _i1.toNoneByDefault(),
+    hideOutput: false,
+    appliesBuilders: const [r'source_gen:part_cleanup'],
+  ),
+  _i1.apply(
+    r'functions_framework_builder:function_framework_builder',
+    [_i3.functionsFrameworkBuilder],
+    _i1.toDependentsOf(r'functions_framework_builder'),
+    hideOutput: false,
+  ),
+  _i1.apply(
+    r'build_resolvers:transitive_digests',
+    [_i4.transitiveDigestsBuilder],
+    _i1.toAllPackages(),
+    isOptional: true,
+    hideOutput: true,
+    appliesBuilders: const [r'build_resolvers:transitive_digest_cleanup'],
+  ),
+  _i1.applyPostProcess(
+    r'build_resolvers:transitive_digest_cleanup',
+    _i4.transitiveDigestCleanup,
+  ),
+  _i1.applyPostProcess(
+    r'source_gen:part_cleanup',
+    _i2.partCleanup,
+  ),
+];
+void main(
+  List<String> args, [
+  _i5.SendPort? sendPort,
+]) async {
+  var result = await _i6.run(
+    args,
+    _builders,
+  );
+  sendPort?.send(result);
+  _i7.exitCode = result;
+}
