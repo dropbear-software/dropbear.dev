de1bdf11c3cbba8defb96ebb128f8f5eff776a27
mark <mark@dropbear.dev>
2024-08-25 16:37:01 +0000

    Add node and lit helpers and update start script

--- www/astro.config.mjs
+++ www/astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
+import lit from "@astrojs/lit";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [lit()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
+});
