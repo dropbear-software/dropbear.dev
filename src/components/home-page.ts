43e6abee7a6ef6155dda8f449f68138f9efd689c
mark <mark@dropbear.dev>
2024-09-20 22:07:07 +0000

    Implement services section

--- src/components/home-page.ts
+++ src/components/home-page.ts
@@ -1,4 +1,4 @@
-import { LitElement, css, html, type CSSResultGroup, type TemplateResult } from "lit";
+import { LitElement, html, type CSSResultGroup, type TemplateResult } from "lit";
 import '@material/web/button/filled-button.js';
 import '@material/web/button/outlined-button.js';
 import '@material/web/labs/card/elevated-card.js';
@@ -6,12 +6,14 @@ import '@material/web/button/filled-tonal-button.js';
 import '../partials/hero/hero-split.js';
 import { customElement } from "lit/decorators.js";
 import { globalStyles } from "../styles/js/global.css.js";
+import { servicesPartial, servicesStyle } from "../partials/services/services-partial.js";
 
 @customElement('home-page')
 export class HomePage extends LitElement {
 
   static override styles: CSSResultGroup = [
     globalStyles,
+    servicesStyle
   ];
 
   protected override render(): TemplateResult {
@@ -21,6 +23,7 @@ export class HomePage extends LitElement {
   #renderHero(): TemplateResult {
     return html`
     <hero-split></hero-split>
+    ${servicesPartial}
     `;
-  }
+  } 
 }
