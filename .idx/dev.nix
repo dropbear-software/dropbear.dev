4534c18e931ffaeb2cb02f7f2980061b9a270bea
mark <mark@dropbear.dev>
2025-01-13 17:19:24 +0000

    Add Lit extension to nix env

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -9,7 +9,7 @@
   env = { };
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
-    extensions = [ "astro-build.astro-vscode" ];
+    extensions = [ "astro-build.astro-vscode"  "runem.lit-plugin"];
     workspace = {
       # Runs when a workspace is first created with this `dev.nix` file
       onCreate = {
