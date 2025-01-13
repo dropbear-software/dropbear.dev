8f1a67bdd4d5d790dca2f1376b5842e733250278
mark <mark@dropbear.dev>
2025-01-13 17:38:03 +0000

    Upgrade dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
-import lit from "@astrojs/lit";
 
 import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [lit()],
+  integrations: [],
   output: "server",
   security: {
     checkOrigin: true
